586e9fd718ef8513aa35664565652add8f0971e8
Tormeti Emmanuel Selase <emmasela04@gmail.com>
2026-07-24 18:11:24 +0000

    test: route barrier-timing diagnostic through logger instead of print

--- scripts/diagnostics/barrier_timing_test.py
+++ scripts/diagnostics/barrier_timing_test.py
@@ -1,9 +1,12 @@
+import logging
 import time
 
 from os_ken.base import app_manager
 from os_ken.controller import ofp_event
 from os_ken.controller.handler import MAIN_DISPATCHER, set_ev_cls
 from os_ken.ofproto import ofproto_v1_3
+
+logger = logging.getLogger(__name__)
 
 
 class BarrierTimingTest(app_manager.OSKenApp):
@@ -12,4 +15,4 @@ class BarrierTimingTest(app_manager.OSKenApp):
 	@set_ev_cls(ofp_event.EventOFPBarrierReply, MAIN_DISPATCHER)
 	def on_barrier_reply(self, ev):
 		dpid = ev.msg.datapath.id
-		print(f'[BARRIER TEST] BarrierReply dpid={dpid} at {time.time():.6f}')
+		logger.warning('[BARRIER TEST] BarrierReply dpid=%s at %.6f', dpid, time.time())
